eced65a6ff29e7be2f6d337221d1015b7c39a71d
chong050 <chong050@morris.umn.edu>
2018-09-10 16:58:00 -0500

    Continued work on getting generation of output working. Currently does not work.

--- bin/process_client_logs.sh
+++ bin/process_client_logs.sh
@@ -1,8 +1,17 @@
 #!/bin/bash
 
-dir = $1
+# dir = $1
+# name = $2
+# cd /lab-1-log-processing-smartbuy/log_files
 
 cd $1
+
 cat var/log/* > temp.txt
-sed -nr 's/[:\d+]//g' < temp.txt > failed_login_data.txt
+
+# Regex removal
+sed -nr 's/[:\d+:\d+\s+\w+\s+\w+\[+\d+\]+:+\s+\w+\s+\w+\s+\w+\s+\w+\s+\w+\s]/ /' < temp.txt > temp.txt
+sed -nr 's/ from / /' < temp.txt > temp.txt
+sed -nr 's/[\s+\w+\s+\d+\s+\w+]//' < temp.txt > failed_login_data.txt
+
+# Cleanup
 rm temp.txt
