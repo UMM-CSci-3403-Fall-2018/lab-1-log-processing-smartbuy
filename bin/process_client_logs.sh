2e6a3ab8f608f754b3b20d022d1cb042b94b4c43
chong050 <chong050@morris.umn.edu>
2018-09-10 16:16:24 -0500

    Changed process_client_logs.sh to a now running state. Need to create a regex to clean logs.

--- bin/process_client_logs.sh
+++ bin/process_client_logs.sh
@@ -1,15 +1,8 @@
 #!/bin/bash
 
 dir = $1
-name = $2
 
 cd $1
-
-# cd /lab-1-log-processing-smartbuy/log_files
-# tar -xcf $2_secure.tgz $1
-
-# or *_secure
-
-# rm `grep '(:\d+)'` > failed_login_data.txt
-
-sed -r 's/[:\d+]//' < $2 > failed_login_data.txt
+cat var/log/* > temp.txt
+sed -nr 's/[:\d+]//g' < temp.txt > failed_login_data.txt
+rm temp.txt
