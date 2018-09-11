fccacd2141cbae11c73e8c97e042754d4db0cfb3
chong050 <chong050@morris.umn.edu>
2018-09-11 11:48:25 -0500

    Changed script to include regex from test environment

--- bin/process_client_logs.sh
+++ bin/process_client_logs.sh
@@ -11,5 +11,15 @@ cat var/log/secure* > temp.txt
 # Clean all data to include only relevent lines
 awk '/Failed password for/ { print $0 }' < temp.txt > temp_clean.txt
 
+# Gets date and hour info and name
+sed -r 's/(:+.+)(user )/ /' < temp_clean.txt > temp_date.txt
+
+# Gets IP
+sed -r 's/\s+from+\s/ /' < temp_date.txt > temp_from.txt
+sed -r 's/\s+port+.+//' < temp_from.txt > failed_login_data.txt
+
 # Cleanup
-# rm temp.txt
+rm temp.txt
+rm temp_clean.txt
+rm temp_date.txt
+rm temp_from.txt
