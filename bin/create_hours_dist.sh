2b3a4425e5b8c705140322c79b1bc0caa1f90b94
chong050 <chong050@morris.umn.edu>
2018-09-13 10:38:06 -0500

    Created create_hours_dist.sh. All tests pass.

---
+++ bin/create_hours_dist.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Create pass call
+here=$(pwd)
+
+# Change to correct directory
+cd $1
+
+# Sort and count the hours and number of failed login attempts
+cat */failed_login_data.txt | awk ' { print $3 } ' > hour.txt
+sort hour.txt > sorted_hour.txt
+uniq -c sorted_hour.txt | awk '{ print "data.addRow([\x27"$2"\x27, "$1"]);" }' > hour_temp.txt
+
+# Run wrap_contents.sh
+$here/bin/wrap_contents.sh hour_temp.txt $here/html_components/hours_dist_header.html $here/html_components/hours_dist_footer.html hours_dist.html
