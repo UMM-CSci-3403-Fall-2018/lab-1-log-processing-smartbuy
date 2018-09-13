d96b3f3f01f8ebe6f1a8eabb144d43e83384a082
chong050 <chong050@morris.umn.edu>
2018-09-12 19:37:54 -0500

    Added comments. Cleaned code. Test for create_username_dist.sh does not pass. Exact same as what was used in test environment. Unsure of cause.

--- bin/create_username_dist.sh
+++ bin/create_username_dist.sh
@@ -1,19 +1,17 @@
 #!/bin/bash
 
+# Create pass call
 here=$(pwd)
 
 # Change to correct directory
 cd $1
 
+# Sort and count the number of user and output to temp.txt
 awk ' { print $4 } ' < failed_login_data.txt > user.txt
 sort user.txt > failed_login_data2.txt
 uniq -c failed_login_data2.txt | awk '{ print "data.addRow([\x27"$2"\x27, "$1"]);" }' > temp.txt
 
-
+# Run wrap_contents.sh 
 $here/bin/wrap_contents.sh temp.txt $here/html_components/username_dist_header.html $here/html_components/username_dist_footer.html username_dist.html
 
-
-# Cleanup
-# rm failed_login_data2.txt
-# rm user.txt
 ~                
--- bin/wrap_contents.sh
+++ bin/wrap_contents.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
+# Take input $1 is file name, $2 and $3 are header and footer, and $4 is output file name
 cat $2 $1 $3 > $4
