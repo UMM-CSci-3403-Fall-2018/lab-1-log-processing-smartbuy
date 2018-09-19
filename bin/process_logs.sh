54d6dce1d2c0b5a3107e99b6206a86a3f3c533c4
miyat001 <miyat001@morris.umn.edu>
2018-09-19 18:22:14 -0500

    I wrote comment on process log sh, but it does not work.

--- bin/process_logs.sh
+++ bin/process_logs.sh
@@ -3,20 +3,21 @@
 # Directory
 here=$(pwd)
 
-# Move to temp directory
+# Move to logfiles directory
 cd $here/log_files
 
 # Loop for extracting contents
+#create tempolary folder for each extracted tar files
+#extrac to that directoly
 for file in $@
 do
 	tempfolder=$(mktemp -d)
-	tar -xvf $file -C $tempfolder
+	tar -xvf $file $tempfolder
 	
-
 done
 
-$here/bin/process_client_logs.sh 
-$here/bin/create_username_dist.sh
-$here/bin/create_hours_dist.sh
-$here/bin/create_country_dist.sh
-$here/bin/assemble_report.sh
+$here/bin/process_client_logs.sh $here/log_files
+$here/bin/create_username_dist.sh $here/log_files
+$here/bin/create_hours_dist.sh $here/log_files
+$here/bin/create_country_dist.sh $here/log_files
+$here/bin/assemble_report.sh $here/log_files
